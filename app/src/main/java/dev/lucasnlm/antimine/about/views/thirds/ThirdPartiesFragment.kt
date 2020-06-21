07d9693b01cd9ff4e85c4837a70f2b8234e19e70
Lucas Lima <me@lucasnlm.dev>
2020-06-21 13:06:30 -0300

    Improve about code

--- app/src/main/java/dev/lucasnlm/antimine/about/views/thirds/ThirdPartiesFragment.kt
+++ app/src/main/java/dev/lucasnlm/antimine/about/views/thirds/ThirdPartiesFragment.kt
@@ -4,22 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-
 import dev.lucasnlm.antimine.R
 import dev.lucasnlm.antimine.about.viewmodel.AboutViewModel
 import kotlinx.android.synthetic.main.fragment_third_party.*
 
 class ThirdPartiesFragment : Fragment() {
-    private var aboutViewModel: AboutViewModel? = null
+    private lateinit var aboutViewModel: AboutViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activity?.run {
-            aboutViewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
+        activity?.let {
+            aboutViewModel = it.viewModels<AboutViewModel>().value
         }
     }
 
@@ -35,12 +34,9 @@ class ThirdPartiesFragment : Fragment() {
 
         licenses.apply {
             setHasFixedSize(true)
-            addItemDecoration(
-                DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
-            )
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
             layoutManager = LinearLayoutManager(context)
+            adapter = ThirdPartyAdapter(aboutViewModel.getLicensesList())
         }
-
-        licenses.adapter = aboutViewModel?.getLicenses()
     }
 }
--- app/src/main/java/dev/lucasnlm/antimine/about/views/translators/TranslatorsFragment.kt
+++ app/src/main/java/dev/lucasnlm/antimine/about/views/translators/TranslatorsFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import dev.lucasnlm.antimine.R
@@ -13,12 +13,12 @@ import dev.lucasnlm.antimine.about.viewmodel.AboutViewModel
 import kotlinx.android.synthetic.main.fragment_translators.*
 
 class TranslatorsFragment : Fragment() {
-    private var aboutViewModel: AboutViewModel? = null
+    private lateinit var aboutViewModel: AboutViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activity?.run {
-            aboutViewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
+        activity?.let {
+            aboutViewModel = it.viewModels<AboutViewModel>().value
         }
     }
 
@@ -33,11 +33,9 @@ class TranslatorsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         translators.apply {
-            addItemDecoration(
-                DividerItemDecoration(view.context, DividerItemDecoration.VERTICAL)
-            )
+            addItemDecoration(DividerItemDecoration(view.context, DividerItemDecoration.VERTICAL))
             layoutManager = LinearLayoutManager(view.context)
-            adapter = aboutViewModel?.getTranslators()
+            adapter = TranslatorsAdapter(aboutViewModel.getTranslatorsList())
         }
     }
 }
